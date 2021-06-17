e503c508238188f816d1a2fd0a185ee4341350b4
HanselV <hanselviva@gmail.com>
2021-06-16 19:42:00 -0700

    added initial login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+
+const initialLoginCredentials = {
+	email: "",
+	password: "",
+};
 
 const Login = () => {
+	const [loginCredentials, setLoginCredentials] = useState(
+		initialLoginCredentials,
+	);
+	const handleChange = (e) => {
+		setLoginCredentials({
+			...loginCredentials,
+			[e.target.name]: e.target.value,
+		});
+	};
+
 	return (
 		<div>
 			<form>
-				<input type="text" />
-				<input type="text" />
+				<input
+					type="text"
+					name="email"
+					value={loginCredentials.email}
+					placeholder="enter your email"
+					onChange={handleChange}
+				/>
+				<input
+					type="text"
+					name="password"
+					value={loginCredentials.password}
+					placeholder="enter your password"
+					onChange={handleChange}
+				/>
+
 				<button> Im an owner </button>
 				<button> Im a renter </button>
 			</form>
