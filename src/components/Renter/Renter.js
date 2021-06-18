b2978763cafce8321f026681acba83f17ace2653
HanselV <hanselviva@gmail.com>
2021-06-17 18:39:47 -0700

    added rented items component

--- src/components/Renter/Renter.js
+++ src/components/Renter/Renter.js
@@ -4,7 +4,8 @@ const Renter = () => {
 	return (
 		<div>
 			This is renter page. items up for rent by owners are displayed here. "rent
-			this" button/functionality should be added{" "}
+			this" button/functionality should be added.
+			<button> View items Im currently renting </button>
 		</div>
 	);
 };
