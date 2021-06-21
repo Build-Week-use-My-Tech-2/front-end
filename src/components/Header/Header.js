3ab82e7cedacad3151883775661f8a78ff97e08e
unknown <seivey@hotmail.com>
2021-06-21 15:23:53 -0600

    basic header with links added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<header>
+        <nav>
+          <a href="/">Home</a>
+		  <a> | </a>
+		  <a href="rentedItems"> Your Current Inventory</a>
+		  <a> | </a>
+		  <a href="/signup"> Sign up </a> <a> or</a>  <a href="/login">Log in</a>
+        </nav>
+      </header>
+
+		)
 };
 
 export default Header;
