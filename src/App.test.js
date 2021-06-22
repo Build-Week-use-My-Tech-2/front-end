438ca9fc2af362060be5136e0ab8a9bf4dfbd185
Stan Tudor <stan2dor@gmail.com>
2021-06-21 21:27:36 -0700

    3 passing tests

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders App without errors', () => {
-  render(<App />);
-  // const h1 = screen.getByText(/use my tech/i)
-  // expect(h1).toBeDefined();
-});
--- src/components/CreateRent/CreateRent.test.js
+++ src/components/CreateRent/CreateRent.test.js
@@ -1,3 +1,7 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import CreateRent from './CreateRent'
+
+test('renders without errors', () => {
+    render(<CreateRent />)
+});
