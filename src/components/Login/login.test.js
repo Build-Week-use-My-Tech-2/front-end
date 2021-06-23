d45a951f73c926a6d0219b04728136244a48a96e
Stan Tudor <stan2dor@gmail.com>
2021-06-22 21:47:49 -0700

    more tests

--- src/components/Login/login.test.js
+++ src/components/Login/login.test.js
@@ -1,8 +1,17 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { BrowserRouter as Router } from "react-router-dom";
 import Login from './Login';
 
 test('renders without error', async () => {
     render(<Router> <Login /> </Router>)
+});
+
+test('render message correctly when isFetchingData is true', () => {
+    render(<Router> <Login isFetchingData={true} /> </Router>)
+
+    const item = screen.queryByText(/dont have an account/i)
+
+    expect(item).not.toBeNull();
+    expect(item).toBeInTheDocument();
 });
