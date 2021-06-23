120da1b49cb448216d8266f03fdbeeb38e2785e5
Stan Tudor <stan2dor@gmail.com>
2021-06-23 10:51:20 -0700

    fixed styled bug and clean up

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,29 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
+// import axios from "axios";
 import styled from "styled-components";
 
-const StyledWrapper = styled.section `
+const StyledWrapper = styled.section`
 	max-width: 1200px;
 	margin: auto;
 `
-
-const StyledBanner = styled.section `
+const StyledBanner = styled.section`
 	display: flex;
 	flex-wrap: wrap;
 	margin: 0em 4.5em;
 `
-
-const StyledPics = styled.img `
-	max-width: 30em;
-	height: auto;
+const StyledPics = styled.img`
+	max-width: 20rem;
+	height: 100%;
 	border-radius: 15%;
 	margin: 2em;
 `
-
-const StyledP = styled.p `
+const StyledP = styled.p`
 	margin: 2em;
-	font-size: 24px;
+	font-size: 21px;
 	line-height: 36px;
 	text-align: justify;
 	height: 344px;
@@ -32,48 +29,23 @@ const StyledP = styled.p `
 	justify-content: center;
 	align-items: center;
 `
-
 const Homepage = () => {
 	const [itemsForRent, setItemsForRent] = useState([]);
 
-	useEffect(() => {
-		axios
-			.get("https://picsum.photos/v2/list")
-			.then((res) => {
-				console.log(res.data);
-				setItemsForRent(res.data);
-			})
-			.catch((err) => console.log(err));
-	}, []);
+	// useEffect(() => {
+	// 	axios
+	// 		.get("https://picsum.photos/v2/list")
+	// 		.then((res) => {
+	// 			console.log(res.data);
+	// 			setItemsForRent(res.data);
+	// 		})
+	// 		.catch((err) => console.log(err));
+	// }, []);
 
 	return (
 		<StyledWrapper>
-			<StyledBanner>
-				<StyledPics src="images/pic02.jpg" />
-				<StyledP>Tired of paying premiums for expensive electornic equipment? Rent it instead!
-					If you use our service you can save thousands a year for equipment you only
-					need once or twice. Check out this cool MacBook Air, it could be yours for
-					the day!
-				</StyledP>
-				<StyledP>Check out this sick drone. You know you want to fly this
-					bad boy. Maybe you want to film a wedding, or scout out some
-					properties. If you rented this, you could be in the air 
-					in no time!
-				</StyledP>
-				<StyledPics src="images/pic03.jpg" />
-				<StyledPics src="images/pic01.jpg" />
-				<StyledP>Maybe you have a disco you're wanting to host? Maybe you
-					have a college party you want to entertain? With this bad boy,
-					you can be making music in no time.
-				</StyledP>
-
-			</StyledBanner>
-			<h1>
-				Use My Tech
-			</h1>
-			<h2>
-				Browse our available Inventory:
-			</h2>
+			<h1>Use My Tech</h1>
+			<h2>Browse our available Inventory:</h2>
 			<div className="wrapper">
 				{itemsForRent.map((item) => {
 					return (
@@ -89,11 +61,31 @@ const Homepage = () => {
 				})}
 			</div>
 			<div>
-				Login button here
+				{/* Login button here */}
 				<Link to="/login">
 					<button> Login </button>
 				</Link>
 			</div>
+			<StyledBanner>
+				<StyledPics src="images/pic02.jpg" />
+				<StyledP>Tired of paying premiums for expensive electornic equipment? Rent it instead!
+					If you use our service you can save thousands a year for equipment you only
+					need once or twice. Check out this cool MacBook Air, it could be yours for
+					the day!
+				</StyledP>
+				<StyledP>Check out this sick drone. You know you want to fly this
+					bad boy. Maybe you want to film a wedding, or scout out some
+					properties. If you rented this, you could be in the air
+					in no time!
+				</StyledP>
+				<StyledPics src="images/pic03.jpg" />
+				<StyledPics src="images/pic01.jpg" />
+				<StyledP>Maybe you have a disco you're wanting to host? Maybe you
+					have a college party you want to entertain? With this bad boy,
+					you can be making music in no time.
+				</StyledP>
+
+			</StyledBanner>
 		</StyledWrapper>
 	);
 };
