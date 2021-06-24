330500144dec42f193452e8bc6902b1ff83f29c1
Drewsteroni <drewsteroni@gmail.com>
2021-06-23 20:53:49 -0700

    header and footer styled

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,38 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledFooter = styled.body `
+display:flex;
+flex-direction: column;
+align-items: center;
+`
+
+const StyledFooting = styled.footer `
+display: flex;
+justify-content: center;
+font-family: trebuchet ms;
+color: black;
+font-size: 1.3em;
+height: 3em;
+width: 3em;
+padding: .2em;
+margin-bottom: 1em;
+background: white;
+`
 
 const Footer = () => {
   return (
     <footer>
-      <nav>
-        <a href="/signup">Copyright © Use My Tech, Inc. All rights reserved </a>
-        {new Date().getFullYear()}
-      </nav>
-    </footer>);
+      <StyledFooter>
+          <StyledFooting>
+				    Use My Tech
+			    </StyledFooting>
+          <nav>
+            <a href="/signup">© {new Date().getFullYear()} Use My Tech, Inc. All Rights Reserved.</a>
+          </nav>
+      </StyledFooter>
+    </footer>
+  );
 };
 
 export default Footer;
