2b0e02503e76dbb756a0356e1908dc0bc1aefbb0
unknown <seivey@hotmail.com>
2021-06-24 21:07:09 -0600

    adding rental data round II

---
+++ src/components/Data.js
@@ -0,0 +1,96 @@
+export default function data = [ 
+    {
+    item:"Camera", 
+    url: "http/camera.jpg", 
+    id: 1, description: "it captures idk", 
+    cost to rent: $1m
+    }   
+
+    {
+    item:"Computer", 
+    url: "http/computer.jpg",
+    id: 2, 
+    description: "idk", 
+    cost to rent: $2m
+    }
+
+    {
+    item:"Fog Machine", 
+    url: "https://static.bhphoto.com/images/images500x500/1404134453_1061775.jpg",
+    id: 3, 
+    description: "The Rosco V-Hazer is a professional-quality fog machine well suited for special events, clubs, and bars, as well as professional film and television productions. The V-Hazer utilizes internal baffles to maximize airflow in order to generate greater output and thorough dispersion of fog particles, ensuring a haze that spreads consistently and smoothly throughout the venue.", 
+    cost to rent: $.5m
+    }
+
+    {
+    item:"Projector", 
+    url: "https://s.yimg.com/aah/focusedtechnology/epson-pro-l12000qnl-black-v11h832820-no-lens-16.jpg",
+    id: 4, 
+    description: "The World's First Native 4K 3LCD 12,000-lumen Laser Projector, the Pro L12000QNL delivers ultra HD image quality for incredible, lifelike performance ? all in a remarkably compact form factor. A groundbreaking large-venue projector, the Pro L12000QNL features astounding 4K resolution (3840 x 2160) for extraordinary detail and 12,000 lumens of color/white brightness1 for vivid color that's sure to amaze.", 
+    cost to rent: $2
+    }   
+
+    {
+    item:"Camera Lenses- Assorted", 
+    url: "https://www.adorama.com/images/Large/caelenskit.jpg",
+    id: 5, 
+    description: "The Canon Cinema Prime CN-E 14mm T3.1 L F (EF mount) Lens offers a compact, consistent lens design and proven Canon lens elements for performance and reliability in professional shooting environments.", 
+    cost to rent: $100k
+    }
+
+    {
+    item:"Video Recorder", 
+    url: "https://static.bhphoto.com/images/images500x500/sony_ilme_fx6vk_fx6_digital_cinema_camera_1605606382_1600161.jpg",
+    id: 6, 
+    description: "Developed to offer versatile, cine-style imaging in a truly compact form, the FX6 Full-Frame Camera Kit from Sony pairs the FX6 camera with the 24-105mm f/4 G OSS E-mount zoom lens. With the ability to capture up to 15+ stops of dynamic range, Sony's S-Cinetone gamma for filmlike skin tones, and up to 10-bit, 4:2:2 XAVC-I recording, the FX6 is poised to both supplement your FX9 or VENICE capture and to nimbly take on documentary, gimbal, and drone shoots on its own. ", 
+    cost to rent: $1.25m
+    }
+
+    {
+    item:"Computer Monitor", 
+    url: "https://cdn.shopify.com/s/files/1/2372/2681/products/MDCC-6530_500x.jpg?v=1620766863",
+    id: 7, 
+    description: "Introducing an energy-efficient and lightweight design for the Barco® Coronis Fusion, the renowned multi-modality display for radiologists. Both smart and bright, Coronis Fusion is designed to help radiologists provide accurate answers.", 
+    cost to rent: $200k
+    }
+
+    {
+    item:"Professional Photography Light Kit", 
+    url: "https://static.bhphoto.com/images/images500x500/ikan_lb10_3pt_kit_lyra_1_x_1_1465293917_1257845.jpg",
+    id:8, 
+    description: "The Lyra 1 x 1 Bi-Color 3-Point Soft Panel LED Light Kit was put together by ikan to offer image-makers a core lighting kit for interviews and ENG on location or in the studio. ", 
+    cost to rent: $150k
+    }
+
+    {
+    item:"Generator", 
+    url: "https://www.electricgeneratorsdirect.com/products-image/600/mlg15_71667_1000_f6bda26895fbac706996e349a20a396d.jpg",
+    id: 9, 
+    description: "", 
+    cost to rent: $2
+    }
+
+    {
+    item:"Television", 
+    url: "https://qvc.scene7.com/is/image/QVC/e/84/e352784.001?$aempdzoom$",
+    id: 10, 
+    description: "TV to the max, the Samsung QN85Q950TS Smart TV offers amazing Real 8K resolution with infinity screen, delivering content in incredibly crisp, vibrant, and life-like clarity and color. Plus, it's equipped with intelligent upscaling, so all of your content looks better than ever before.", 
+    cost to rent: $2
+    }
+
+    {
+    item:"Party Light Kit", 
+    url: "https://cdn11.bigcommerce.com/s-reotp/images/stencil/1200x1200/products/7820/7848372/1583444__03919.1624027938.jpg?c=2",
+    id: 11, 
+    description: "Includes 24 Rockville RockPAR50 LED RGB Compact Par Can DJ/Club DMX Wash Light+Dual Bracket", 
+    cost to rent: $2
+    }
+
+    {
+    item:"DDR", 
+    url: "https://www.thepinballcompany.com/wp-content/uploads/2017/02/Pump-it-up-20th-510x510.jpg",
+    id: 12, 
+    description: "World’s No.1 Dance Simulation Game! New brilliant light show upgrades any location! Unleash the wall of sound with 4 speakers and 2 subwoofers that blast the jams. HD graphics POP on a mesmerizing, huge 55” screen.", 
+    cost to rent: $2
+    }
+]
